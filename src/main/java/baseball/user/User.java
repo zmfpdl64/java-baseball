e3ea5ad8934b51cf2a8f47287dfc7c8ab7c69978
leewoojin <zmfpdl64@naver.com>
2022-11-02 20:45:47 +0900

    dev: 게임장, 사용자, 상대방 객체 생성
    게임을 하기 위해 필요한 객체들을 생성했다.

---
+++ src/main/java/baseball/user/User.java
@@ -0,0 +1,13 @@
+package baseball.user;
+
+public class User {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
