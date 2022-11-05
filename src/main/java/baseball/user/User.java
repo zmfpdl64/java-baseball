cb3514c3f0d79a0afd55fe8590e4f70467dce1d2
leewoojin <zmfpdl64@naver.com>
2022-11-05 20:51:46 +0900

    dev: User객체 입력값 받기 기능 추가
    추가된 기능
    1. 입력된 값을 받아 filed number에 저장한다.(유효성 검사 가능)

--- src/main/java/baseball/user/User.java
+++ src/main/java/baseball/user/User.java
@@ -1,13 +1,23 @@
 package baseball.user;
 
+import baseball.valid.Valid;
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
+
     private int number;
 
-    public int getNumber() {
+    public int inputNumber() {
+        int number = -1;
+        while(number == -1){
+            number = Valid.userInput(Console.readLine());
+        }
+        this.number = number;
         return number;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+
+    public int getNumber() {
+        return number;
     }
 }
