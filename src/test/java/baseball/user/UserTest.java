69665a3a1fb7f9053a1eb9d344717843bc0636e9
leewoojin <zmfpdl64@naver.com>
2022-11-05 20:54:57 +0900

    test: User의 기능 검증
    inputNumber 메소드 검증을 해주었다.
    
    Valid에서 유효성 검증을 진행하기 때문에 Valid객체를 생성해서 진행했다.
    
    1. 입력받은 값이 3자리가 아닐 때
    2. 입력받은 값이 정수가 아닐 때
    3. 중복된 입력값이 존재할 때

---
+++ src/test/java/baseball/user/UserTest.java
@@ -0,0 +1,54 @@
+package baseball.user;
+
+import baseball.valid.Valid;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.in;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("유저 클래스 테스트")
+class UserTest {
+
+    @DisplayName("유저한테 입력 받기")
+    @Test
+    void 유저한테_정상적인_값_입력받기(){
+        User user = new User();
+        int number = user.inputNumber();
+        assertThat(number).isBetween(100, 999);
+    }
+    @DisplayName("유저 입력값 예외 발생1")
+    @Test
+    void 유저_입력값_길이가_3이_아닐때_예외_발생(){
+        String input = "12";
+        assertThatThrownBy(() -> Valid.OutOfRange(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자의 범위를 벗어났습니다. 다시 입력해주세요");
+    }
+    @DisplayName("유저 입력값 예외 발생2")
+    @Test
+    void 유저_입력값_정수가_아닐때_예외_발생(){
+        Character input = 'a';
+        assertThatThrownBy(() -> Valid.NotMatch(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("정수로 다시 입력해주세요");
+    }
+    @DisplayName("유저 입력값 예외 발생3")
+    @Test
+    void 유저_입력값_중복된_값_존재시_예외_발생(){
+        Character input = '1';
+        List<Character> list = new ArrayList<>(List.of('1','2'));
+        assertThatThrownBy(() -> Valid.checkOverlap(list, input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 숫자가 들어 왔습니다. 다시 입력해주세요");
+    }
+
+}
