df609ee9bd2a8000058ac87057f63dc5861f3ba1
leewoojin <zmfpdl64@naver.com>
2022-11-05 18:49:01 +0900

    dev: 상대방(컴퓨터)가 필요한 메서드 구현
    1. 1 ~ 9까지의 난수 발생 함수
    2. 중복되지 않게 3자리의 난수 생성
    
    setter는 데이터의 안전성 때문에 제거함

--- src/main/java/baseball/user/Opponent.java
+++ src/main/java/baseball/user/Opponent.java
@@ -1,13 +1,42 @@
 package baseball.user;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Opponent {
-    private int number;
+    private final int number;
+    private static final int MAX = 9;
+    private static final int MIN = 1;
+    protected static final int SIZE = 3;
+
+    public Opponent() {
+        this.number = checkOverlap();
+    }
+
+    private static int createNums(){  //중복하지 않는 수를 반환한다.
+        return Randoms.pickNumberInRange(MIN, MAX);
+    }
+    public static int checkOverlap() {//중복되지 않는 정수를 반환
+        List<Integer> nums = new ArrayList<>();
+        int result = 0;
+        while(nums.size() < SIZE) {
+            int num = createNums();
+            if(!nums.contains(num)){
+                nums.add(num);
+            }
+        }
+        for(int i = 0; i < SIZE; i++){  //각 자릿수 합치기
+            result += nums.get(i)*Math.pow(10, i);
+        }
+
+        return result;
+    }
 
     public int getNumber() {
         return number;
     }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
 }
+
