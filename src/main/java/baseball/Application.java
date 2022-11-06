55eb37932e546c89ac506e4cde0e3641f7b45fd2
leewoojin <zmfpdl64@naver.com>
2022-11-06 19:26:06 +0900

    docs: Valid, User 객체 기능 추가
    추가:
    Valid :
        유저에게 다시 게임 할건지 물어보는 함수 유효성 검사
    
    User :
        유저에게 다시 게임할건지 물어보는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.game.BaseBall;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBall game = new BaseBall();
+        game.playGame();
     }
 }
