777ba1f12b6dbca8fda4dfee8739d50fb956a1c4
leewoojin <zmfpdl64@naver.com>
2022-11-06 19:22:26 +0900

    dev: 유저에게 restart 입력받는 메소드 추가
    기능:
    Valid를 통해 유효성 검사를 하고 사용자에게서 1 또는 2만 입력받을 수 있다.

--- src/main/java/baseball/user/User.java
+++ src/main/java/baseball/user/User.java
@@ -16,6 +16,15 @@ public class User {
         return number;
     }
 
+    public Boolean restart() {
+        int number = -1;
+        while(number != 1 && number != 2) {
+            number = Valid.userRestart(Console.readLine());
+        }
+        if(number == 1) return true;
+        else return false;
+    }
+
 
     public int getNumber() {
         return number;
