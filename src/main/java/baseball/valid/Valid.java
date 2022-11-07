65e1e344ad13d88a8651b5dceca3f68b45ca7538
leewoojin <zmfpdl64@naver.com>
2022-11-07 13:30:44 +0900

    dev fix: 예외 발생시 종료로 변경(기존 다시 입력이었음)
    다시 입력 받는 형식에서 종료 하는 것으로 변경

--- src/main/java/baseball/valid/Valid.java
+++ src/main/java/baseball/valid/Valid.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 public class Valid {
 
     public static int userInput(String num){
-        try{
             OutOfRange(num); //입력값의 범위를 벗어 났을 때
             List<Character> list = new ArrayList<>();
             for(int i = 0; i < num.length(); i++){
@@ -16,41 +15,30 @@ public class Valid {
                 checkOverlap(list, c); // 숫자가 중복됬을 때
                 list.add(c);
             }
-
             return Integer.parseInt(num);
-
-        }catch(IllegalArgumentException e){
-            System.out.printf("%s", e);
-            return -1;
-        }
     }
     public static void OutOfRange(String num) {
         if(num.length() != 3){
-            throw new IllegalArgumentException("숫자의 범위를 벗어났습니다. 다시 입력해주세요 :");
+            throw new IllegalArgumentException("숫자의 범위를 벗어났습니다.");
         }
     }
     public static void NotMatch(Character character) {
         if(!Character.isDigit(character)){ //입력 받은 값이 숫자가 아닐 때
-            throw new IllegalArgumentException("정수로 다시 입력해주세요 :");
+            throw new IllegalArgumentException("입력된 값이 정수가 아닙니다.");
         }
     }
     public static void checkOverlap(List<Character> list, Character character){
         if(list.contains(character)){       //입력 받은 값이 동일한 값이 들어 있을 때
-            throw new IllegalArgumentException("중복된 숫자가 들어 왔습니다. 다시 입력해주세요 :");
+            throw new IllegalArgumentException("중복된 숫자가 들어 왔습니다.");
         }
     }
 
     public static Integer userRestart(String desicion){
-        try{
             String input_regex = "[1-2]{1}"; //한글자이며 숫자여야한다.
             if(!Pattern.matches(input_regex, desicion)){
-                throw new IllegalArgumentException("입력된 값이 조건에 맞지 않습니다. 다시 입력해주세요: ");
+                throw new IllegalArgumentException("입력된 값이 조건에 맞지 않습니다.");
             }
             return Integer.valueOf(desicion);
-        }catch(IllegalArgumentException e){
-            System.out.printf("%s",e);
-            return -1;
-        }
     }
 
 }
