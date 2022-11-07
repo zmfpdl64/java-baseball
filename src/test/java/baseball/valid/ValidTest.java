e3218427013efe37ff36f63038045bdf49162d7d
leewoojin <zmfpdl64@naver.com>
2022-11-07 15:33:07 +0900

    test : Valid 테스트 DisplayName 추가

--- src/test/java/baseball/valid/ValidTest.java
+++ src/test/java/baseball/valid/ValidTest.java
@@ -2,6 +2,7 @@ package baseball.valid;
 
 import baseball.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -20,12 +21,16 @@ class ValidTest extends NsTest {
         );
     }
 
+    @DisplayName("예외: 숫자가 아닌값")
+    @Test
     void 숫자가_아닌값_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("a23"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @DisplayName("예외: 숫자가 중복된 값")
     @Test
     void 중복된값_예외_테스트() {
         assertSimpleTest(() ->
@@ -34,6 +39,7 @@ class ValidTest extends NsTest {
         );
     }
 
+    @DisplayName("예외: 재시작 입력값 범위 벗어남")
     @Test
     void 재시작_입력값_범위_예외_테스트() {
         assertRandomNumberInRangeTest(
@@ -44,6 +50,8 @@ class ValidTest extends NsTest {
                 1, 3, 5, 5, 8, 9
         );
     }
+
+    @DisplayName("예외: 재시작 입력값 정수 아님")
     @Test
     void 재시작_입력값_정수아닌값_예외_테스트() {
         assertRandomNumberInRangeTest(
