eeb792167934b11a6a336b243ce34d48b9dfe36f
leewoojin <zmfpdl64@naver.com>
2022-11-07 13:52:30 +0900

    test: Valid test 파일 생성

---
+++ src/test/java/baseball/valid/ValidTest.java
@@ -0,0 +1,20 @@
+package baseball.valid;
+
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidTest extends NsTest {
+
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
